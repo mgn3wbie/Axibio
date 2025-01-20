7856361a2713bd3382237392b40003ead3cda7a2
n3wbie <julien_gil@outlook.com>
2025-01-20 23:48:27 +0100

    creation of a react app

---
+++ services/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Homepage from './components/Homepage';
+import Navbar from './components/Navbar';
+import './App.css'; // Import the styles.css file
+
+// main application component and configuration of the routes using `react-router-dom`.
+function App() {
+  return (
+    <Router>
+    <Navbar />
+    <div className="container">
+    <Routes>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    </Routes>
+    </div>
+    </Router>
+  );
+}
+export default App;
